14d1d91d60a4b1e1c00881c7a94eb98c3f47ca4c
datny <robert.przydatek.cl@gmail.com>
2019-07-08 19:29:36 +0200

    pairs second approach

--- pairs.py
+++ pairs.py
@@ -14,6 +14,23 @@ def pair_sum(arr, k):
                         pairs.append((arr[indx], arr[indx2]))
     return print(pairs)
 
+
+def pair_sum2(arr, k):
+    if (len(arr) < 2):
+        return
+
+    seen = set()
+    output = set()
+
+    for num in arr:
+        target = k - num
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add((min(num, target), max(num, target)))
+    print(output)
+
+
 pair_sum([1,3,2,2],4)
 pair_sum([1,9,2,8,3,7,4,6,5,5,13,14,11,13,-1],10)
 pair_sum([1,2,3,1],3)
